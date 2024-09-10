20b0946d0cd4953389e8a01e9ee82244f55f90e1
karan <karan@example.com>
2024-09-10 12:45:31 +0530

    js

---
+++ class1.js
@@ -0,0 +1,29 @@
+
+
+// console.log("hello")
+// console.log(123)
+// console.log(2+10)
+// console.log(10*10)
+
+
+// var a =10;
+// var b =20;
+
+// console.log(a*b);
+// console.log(a+b);
+// console.log(b-a);
+// console.log
+
+
+// var a=10;
+// var a=20;
+// var b=20;
+
+// let a = 20;
+//     a = 30;
+// console.log(a);
+
+// const a = 10;
+//       a = 20;
+
+// console.log(a);
